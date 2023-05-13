a7056b39b798aab17327f34994b6ebcdd0320d04
Amir Delta <88898251+Amir-Delta@users.noreply.github.com>
2023-05-13 13:53:41 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function submitForm() {
+    
+    var recomText = document.getElementById("form_text").value;
+
+    var newRecom = document.createElement("div");
+    var newRecomText = document.createTextNode("\" "+recomText+" \"");
+    var newP = document.createElement("p");
+
+    newP.appendChild(newRecomText);
+    document.getElementById("recomcontainterid").appendChild(newRecom).appendChild(newP).className="recom";
+}
+
+function confirmMessage() { 
+    var recomName =document.getElementById("form_name").value;
+        if (recomName != "") {
+            document.getElementById("messageText").innerHTML="Thanks "+recomName+" for leaving a recommendation."
+            document.getElementById("messageText").setAttribute("style","font-family: Verdana, Geneva, Tahoma, sans-serif;color:rgb(100,100,100);text-align: center;");}
+        else {
+            document.getElementById("messageText").innerHTML="Thanks for leaving a recommendation."
+            document.getElementById("messageText").setAttribute("style","font-family: Verdana, Geneva, Tahoma, sans-serif;color:rgb(100,100,100);text-align: center;");
+        }
+            
+}
+
+function clearForms() { 
+    recomName =document.getElementById("form_name").value=""
+    recomName =document.getElementById("form_text").value=""
+}
